58e82aa007a79f4c3a9c79637d760f561ebb438e
Maksut Çakmaktaş <100886044+Diandyistaken@users.noreply.github.com>
2024-01-06 09:09:48 +0300

    Cone of influence
    In this commit; let's also set it up so that we can change the angle of influence in the ispector to dedicate not only the angle on which the projectiles come out, but how many projectiles comes out in each bursts.

--- Assets/Scripts/Enemies/Shooter.cs
+++ Assets/Scripts/Enemies/Shooter.cs
@@ -8,6 +8,9 @@ public class Shooter : MonoBehaviour, IEnemy
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletMoveSpeed;
     [SerializeField] private int burstCount;
+    [SerializeField] private int projectilesPerBurst;
+    [SerializeField][Range(0,359)] private float angleSpread;
+    [SerializeField] private float startingDistance = 0.1f;
     [SerializeField] private float timeBetweenBursts;
     [SerializeField] private float restTime = 1f;
 
@@ -23,27 +26,68 @@ public class Shooter : MonoBehaviour, IEnemy
         
     }
 
-    private IEnumerator ShootRoutine() { 
-        isShooting=true;
+    private IEnumerator ShootRoutine()
+    {
+        isShooting = true;
+        float startAngle, currentAngle, angleStep;
+        TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep);
 
         for (int i = 0; i < burstCount; i++)
         {
-            
-            Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
-        
-            GameObject newBullet = Instantiate(bulletPrefab,transform.position,Quaternion.identity);
-            newBullet.transform.right = targetDirection;
+            for (int j = 0; j < projectilesPerBurst; j++)
+            {
+
+                Vector2 pos = FindBulletSpawnPos(currentAngle);
 
 
-            // bunu yapmamizin sebebi take a method or a function pass through a parameter, and then access it outside of the function. 
-            if(newBullet.TryGetComponent(out Projectile projectile)) { 
-                projectile.UpdateMoveSpeed(bulletMoveSpeed);
-            
+                GameObject newBullet = Instantiate(bulletPrefab, pos, Quaternion.identity);
+                newBullet.transform.right = newBullet.transform.position - transform.position;
+
+
+                // bunu yapmamizin sebebi take a method or a function pass through a parameter, and then access it outside of the function. 
+                if (newBullet.TryGetComponent(out Projectile projectile))
+                {
+                    projectile.UpdateMoveSpeed(bulletMoveSpeed);
+
+                }
+                currentAngle += angleStep;
             }
+
+            currentAngle = startAngle;
+
             yield return new WaitForSeconds(timeBetweenBursts);
+            TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep);
         }
 
         yield return new WaitForSeconds(restTime);
         isShooting = false;
+    }
+
+    private void TargetConeOfInfluence(out float startAngle, out float currentAngle, out float angleStep)
+    {
+        Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
+        float targetAngle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
+        startAngle = targetAngle;
+        float endAngle = targetAngle;
+        currentAngle = targetAngle;
+        float halfAngleSpread = 0f;
+        angleStep = 0;
+        if (angleSpread != 0)
+        {
+            angleStep = angleSpread / (projectilesPerBurst - 1);
+            halfAngleSpread = angleSpread / 2f;
+            startAngle = targetAngle - halfAngleSpread;
+            endAngle = targetAngle + halfAngleSpread;
+            currentAngle = startAngle;
+        }
+    }
+
+    private Vector2 FindBulletSpawnPos(float currentAngle)
+    {
+        float x = transform.position.x + startingDistance * Mathf.Cos(currentAngle * Mathf.Deg2Rad);
+        float y = transform.position.y + startingDistance * Mathf.Sin(currentAngle * Mathf.Deg2Rad);
+        Vector2 pos = new Vector2(x, y);
+
+        return pos; 
     }
 }
