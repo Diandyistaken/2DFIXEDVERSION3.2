ec6a8936455f8dcef9bf776c91b3c167159b23e4
Maksut Çakmaktaş <100886044+Diandyistaken@users.noreply.github.com>
2024-01-06 09:54:41 +0300

    Oscillate & Stagger
    I;d like to add a little bit more depth to our shooter clkass, adding both oscillate and stagger properties

--- Assets/Scripts/Enemies/Shooter.cs
+++ Assets/Scripts/Enemies/Shooter.cs
@@ -13,6 +13,10 @@ public class Shooter : MonoBehaviour, IEnemy
     [SerializeField] private float startingDistance = 0.1f;
     [SerializeField] private float timeBetweenBursts;
     [SerializeField] private float restTime = 1f;
+    [SerializeField] private bool stagger;
+    [SerializeField] private bool oscillate;
+
+
 
     private bool isShooting = false;
 
@@ -29,11 +33,37 @@ public class Shooter : MonoBehaviour, IEnemy
     private IEnumerator ShootRoutine()
     {
         isShooting = true;
-        float startAngle, currentAngle, angleStep;
-        TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep);
+        float startAngle, currentAngle, angleStep, endAngle;
+        float timeBetweenProjectiles = 0f;
+
+        TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep, out endAngle);
+
+        if (stagger) { timeBetweenProjectiles = timeBetweenBursts / projectilesPerBurst; }
 
         for (int i = 0; i < burstCount; i++)
         {
+            if(!oscillate) { 
+                TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep, out endAngle);
+            }
+
+
+
+            if(oscillate && i %2 != 1)
+            {
+                TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep, out endAngle);
+            }
+
+            else if (oscillate)
+            {
+                currentAngle = endAngle;
+                endAngle = startAngle;
+                startAngle = currentAngle;
+                angleStep *= -1;
+
+            }
+
+            
+
             for (int j = 0; j < projectilesPerBurst; j++)
             {
 
@@ -51,24 +81,27 @@ public class Shooter : MonoBehaviour, IEnemy
 
                 }
                 currentAngle += angleStep;
+
+                if (stagger) { yield return new WaitForSeconds(timeBetweenProjectiles); }
             }
 
             currentAngle = startAngle;
 
-            yield return new WaitForSeconds(timeBetweenBursts);
-            TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep);
+            if(!stagger) {  
+                yield return new WaitForSeconds(timeBetweenBursts);
+            }
         }
 
         yield return new WaitForSeconds(restTime);
         isShooting = false;
     }
 
-    private void TargetConeOfInfluence(out float startAngle, out float currentAngle, out float angleStep)
+    private void TargetConeOfInfluence(out float startAngle, out float currentAngle, out float angleStep, out float endAngle)
     {
         Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
         float targetAngle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
         startAngle = targetAngle;
-        float endAngle = targetAngle;
+        endAngle = targetAngle;
         currentAngle = targetAngle;
         float halfAngleSpread = 0f;
         angleStep = 0;
