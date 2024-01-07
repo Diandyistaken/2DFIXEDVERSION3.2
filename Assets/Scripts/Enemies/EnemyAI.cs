fbf10c08b0c410ffbbf594c01176f0ab856483f8
Maksut Çakmaktaş <100886044+Diandyistaken@users.noreply.github.com>
2024-01-07 10:45:08 +0300

    grape setup
    I just add a new grape animations using slime sheets. Now it can moving around and can attack if you get in of his range.

--- Assets/Scripts/Enemies/EnemyAI.cs
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -6,7 +6,7 @@ public class EnemyAI : MonoBehaviour
 {
 
     [SerializeField] private float roamChangeDirFloat = 2f;
-    [SerializeField] private float attackRange = 5f;
+    [SerializeField] private float attackRange = 0f;
     [SerializeField] private MonoBehaviour enemyType;
     [SerializeField] private float attackCooldown = 2f;
     [SerializeField] private bool stopMovingWhileAttacking = false;
