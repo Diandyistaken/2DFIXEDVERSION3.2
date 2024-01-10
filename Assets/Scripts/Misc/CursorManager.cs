4cb62d62f251b7f0b43779d5d35a6d0bdaa3140f
Maksut Çakmaktaş <100886044+Diandyistaken@users.noreply.github.com>
2024-01-10 03:10:35 +0300

    before taner

---
+++ Assets/Scripts/Misc/CursorManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+
+
+        if(Application.isPlaying)
+        {
+            Cursor.lockState = CursorLockMode.None;
+
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+    }
+}
